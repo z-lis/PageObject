21f0a05e742b76c7fa94c410fcdc28981cea05b9
z-lis <zakharlisica@rambler.ru>
2022-02-21 15:17:03 +0300

    added is element presented method

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage():
 
     def __init__(self, browser, url, timeout=10):
@@ -7,3 +10,10 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -8,14 +8,7 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
-
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
 
 
-#link = "http://selenium1py.pythonanywhere.com/"
-
-
-#def test_guest_can_go_to_login_page(browser):
-#   browser.get(link)
-#    go_to_login_page(browser)
