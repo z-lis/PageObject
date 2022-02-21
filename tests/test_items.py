cc6fdbb4ef163ab4cb793b383d4f41785cd89064
z-lis <zakharlisica@rambler.ru>
2022-02-21 12:02:08 +0300

    added skeleton files

--- tests/test_items.py
+++
@@ -1,6 +0,0 @@
-def test_add_to_basket_button(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    add_button = len(browser.find_elements_by_css_selector("button.btn-add-to-basket"))
-
-    assert add_button > 0, 'Элемент не найден'
